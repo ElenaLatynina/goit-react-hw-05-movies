d88f8c86245a5f5911140d8f3c831b3c989b627f
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-09 11:37:45 +0200

    App

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
+import { Route, Routes } from 'react-router-dom';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Routes>
+      <Route path="/" element={<SharedLayout />}>
+        <Route index element={<Home />}></Route>
+        <Route path="/movies" element={<Movies />}></Route>
+        <Route path="/movies/:movieId" element={<MovieDetails />}>
+          <Route path="cast" element={<Cast />}></Route>
+          <Route path="reviews" element={<Reviews />}></Route>
+        </Route>
+        <Route path="*" element={<NotFound />}></Route>
+        </Route>
+    </Routes>
   );
 };
