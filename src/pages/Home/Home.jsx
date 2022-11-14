97392b9990cfea16b34bc302fb72f48d4ad6f075
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-14 22:25:23 +0200

    Home

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,27 @@
+import { useState, useEffect } from "react";
+import { getTrendingFilms } from "components/Services/GetFilms";
+import { NavLink } from 'react-router-dom';
 
-
+const axios = require('axios').default;
 
 const Home = () => {
-    
+    const [films, setFilms] = useState([]);
+
+    useEffect(() => {
+        getFilms();
+
+        async function getFilms() {
+            try {
+                const url = getTrendingFilms();
+                const response = await axios.get(url);
+
+                const { data: { results }, } = response;
+                setFilms(results);
+            } catch (error) {
+                console.log(error);
+            }
+        }
+    }, []);
 
 
     return (
@@ -11,7 +30,12 @@ const Home = () => {
                 Trending today
             </h2>
             <ul>
-                
+                {films.map(({ id, title }) => (
+                    <li key={id}>
+                        <NavLink to={`/movies/${id}`}>{ title}</NavLink>
+                        
+                    </li>
+                ))}
             </ul>
         </main>
     );
