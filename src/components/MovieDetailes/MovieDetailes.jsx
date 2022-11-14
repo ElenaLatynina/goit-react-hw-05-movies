633778642472215eabf36afab9fe778d226840f3
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-14 20:39:09 +0200

    MovieDetailes

---
+++ src/components/MovieDetailes/MovieDetailes.jsx
@@ -0,0 +1,35 @@
+
+
+
+const MovieDetailes = () => {
+    
+
+    return (
+        <>
+            <button>
+                Back
+            </button>
+            <div>
+                <img src={ } alt={ }></img>
+                <div>
+                    <h2>Title Year</h2>
+                    <p> User score</p>
+                    <p>Overview</p>
+                    <h3>Genres
+                        <ul> Genres List</ul>
+                    </h3>
+
+                </div>
+            </div>
+            <div>
+                <h3>Additional Information</h3>
+                <ul>
+                    <li>Cast</li>
+                    <li>Reviews</li>
+                </ul>
+            </div>
+        </>
+    );
+};
+
+export default MovieDetailes;
