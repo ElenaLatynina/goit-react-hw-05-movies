3cf3bbb05e8bb82bfc34fea42438b3687e785936
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-19 16:09:27 +0200

    Cast fix-2

--- src/components/Cast/Cast.jsx
+++ src/components/Cast/Cast.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 // import { getActors } from '../Services/GetFilms';
-import { Image } from './Cast.styled';
+import { ActorsList, Image, ActorCard, Info, Name, Character } from './Cast.styled';
 // const axios = require('axios').default;
 
 
 const API_KEY = '07365d3730901c9189566ffe38d9d5bb';
 const BASE_URL = 'https://api.themoviedb.org/3/';
+const placeHolder =
+  'https://upload.wikimedia.org/wikipedia/commons/8/89/Portrait_Placeholder.png';
 
 
 const Cast = () => {
@@ -14,7 +16,7 @@ const Cast = () => {
     const { Id } = useParams();
 
     useEffect(() => {
-        const url = `${BASE_URL}movie/${id}/credits?api_key=${API_KEY}&language=en-US`;
+        const url = `${BASE_URL}movie/${Id}/credits?api_key=${API_KEY}&language=en-US`;
 
         function getActors() {
             fetch(url).then(response => response.json())
@@ -34,19 +36,24 @@ const Cast = () => {
     getActors();},[Id]);
 
     return (
-        actors.map(({ name, character, poster }) => { 
-            return (
-                <li key={name}>
-                    <Image src={poster} alt={name} widtn="150px" height="200px" />
-                    <div>
-                    <p>{name}</p>
-                    <p>Character:{character}</p>
-                    </div>
-                </li>
-            )
-        }
+        <ActorsList>
+            {actors.map(({ name, character, poster }) => {
+                return (
+                    <ActorCard key={name}>
+                        <Image src={poster} alt={name} widtn="150px" height="200px" />
+                        <Info>
+                            <Name>{name}</Name>
+                            <Character>Character:{character}</Character>
+                        </Info>
+                    </ActorCard>
+                );
+            }
            
-        ))
+            )}
+               </ActorsList>)
+
+     
+    
 };
 
 export default Cast;
--- src/components/Cast/Cast.styled.js
+++ src/components/Cast/Cast.styled.js
@@ -5,3 +5,35 @@ width:120px;
 height: 180px;
 
 `;
+export const ActorsList = styled.ul`
+display: flex;
+  flex-wrap: wrap;
+  gap: 30px;
+  align-items: center;
+  justify-content: center;
+  list-style: none;
+
+`;
+
+
+export const ActorCard = styled.li`
+flex-basis: calc(12% - 30px);
+  :not(:last-child) {
+    margin-bottom: 30px;
+  }
+
+`;
+
+export const Info = styled.div`
+  flex-grow: 1;
+
+`;
+
+export const Name = styled.h2`
+font-size: 16px;
+margin-bottom: 8px;
+`
+
+export const Character = styled.span`
+  font-weight: bold;
+`;
