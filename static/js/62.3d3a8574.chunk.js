398c097f125e7e961a794ba542b9bbfe5b6af98b
ElenaLatynina <96691226+ElenaLatynina@users.noreply.github.com>
2022-11-19 18:49:34 +0000

    Deploying to gh-pages from @ ElenaLatynina/goit-react-hw-05-movies@263915022390560cb4707e2f20940ac1d1678cac 🚀

---
+++ static/js/62.3d3a8574.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[62],{62:function(e,n,t){t.r(n),t.d(n,{default:function(){return h}});var r,o=t(731),c=t(168),i=t(444).ZP.div(r||(r=(0,c.Z)(["\ntext-align: center;\ncolor: red;\nfont-size: 16px;\nfont-weight: 500;\n\n"]))),a=t(184),h=function(){return(0,a.jsxs)(i,{children:[(0,a.jsx)("p",{children:"Sorry, there is no page"}),(0,a.jsx)(o.OL,{to:"/",children:"Go to the main page"})]})}}}]);
+//# sourceMappingURL=62.3d3a8574.chunk.js.map
