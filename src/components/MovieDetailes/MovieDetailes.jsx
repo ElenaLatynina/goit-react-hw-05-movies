678facc93dcbae0c1ef56d0542f389c1e3e0170c
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-19 19:29:54 +0200

    fixinx derailes

--- src/components/MovieDetailes/MovieDetailes.jsx
+++ src/components/MovieDetailes/MovieDetailes.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect, Suspense } from 'react';
-import { useParams , useLocation, NavLink, Outlet} from 'react-router-dom';
-import { ReturnButton,MovieContainer, MovieInfo, Poster, Score, Title, Overview, Genres } from './MovieDetailes.styled';
+import { useParams , useLocation, Link, Outlet} from 'react-router-dom';
+import { ReturnButton, MovieContainer } from './MovieDetailes.styled';
+import { MovieItem } from 'components/MovieItem/MovieItem';
 // import { getDescription } from '../Services/GetFilms';
 // const axios = require('axios').default;
 
@@ -13,15 +14,14 @@ const MovieDetailes = () => {
     const location = useLocation();
     const backLink = location.state?.from ?? '/movies';
 
-    useEffect = (() => {
+    useEffect(() => {
         const url = `${BASE_URL}movie/${id}?api_key=${API_KEY}&language=en-US`;
         
         function fetchMovie() {
-            fetch(url)
-                .then(response => response.json())
+            fetch(url).then(response => response.json())
                 .then(data => {
                     setMovie({
-                        poster: `https://image.tmdb.org/t/p/w500/${data.poster}`,
+                        poster: `https://image.tmdb.org/t/p/w500/${data.poster_path}`,
                         title: data.title,
                         score: Number.parseInt(data.vote_average * 10),
                         overview: data.overview,
@@ -32,43 +32,43 @@ const MovieDetailes = () => {
                 })
                 .catch(error => console.log(error));
         }
-    
         fetchMovie();
     }, [id]);
 
     return (
-            <MovieContainer>
-                <ReturnButton to={backLink}>
-                    Back
-                </ReturnButton>
-                <Poster src={movie.poster } alt={movie.title} width="240"  height="320"/>
-                <MovieInfo>
-                    <Title>{movie.title}</Title>
-                    <Score>User Score: {movie.score}%</Score>
-                    <h3>Overview</h3>
-                    <Overview>{movie.overview}</Overview>
-                    <Genres>Genres</Genres>
-                    <p>{ movie.genres}</p>
-                    
-                </MovieInfo>
+        <MovieContainer>
+            <ReturnButton to={backLink}>
+                Back
+            </ReturnButton>
+                
+            <MovieItem
+                poster={movie.poster}
+                title={movie.title}
+                score={movie.score}
+                overview={movie.overview}
+                genres={movie.genres}
+            />
            
-                <div>
-                    <h3>Additional Information</h3>
-                    <ul>
-                        <li>
-                            <NavLink to="cast" state={{ from: backLink }}>Cast</NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="reviews" state={{ from: backLink }}>Reviews</NavLink>
-                        </li>
-                    </ul>
-                    <Suspense fallback={<div>Is loading</div>}>
-                        <Outlet />
-                    </Suspense>
-                </div>
-            </MovieContainer>
+            <div>
+                <h3>Additional Information</h3>
+                <ul>
+                    <li>
+                        <Link to="cast" state={{ from: backLink }}>Cast</Link>
+                    </li>
+                    <li>
+                        <Link to="reviews" state={{ from: backLink }}>Reviews</Link>
+                    </li>
+                </ul>
+                <Suspense fallback={<div>Is loading</div>}>
+                    <Outlet />
+                </Suspense>
+            </div>
+        </MovieContainer>
 
-        );
+    );
+    
 
-}
+};
+
+
 export default MovieDetailes;
--- src/components/MovieItem/MovieItem.jsx
+++ src/components/MovieItem/MovieItem.jsx
@@ -5,7 +5,7 @@ export const MovieItem = ({ poster, title, score, overview, genres }) => {
     return (
         <MovieContainer>
             <div>
-            <Poster src={poster } alt={title} width="240"  height="320"/>
+                <Poster src={poster} alt={title} width="240" height="320" />
             </div>
             <MovieInfo>
                 <Title>{title}</Title>
@@ -13,8 +13,8 @@ export const MovieItem = ({ poster, title, score, overview, genres }) => {
                 <Title>Overview</Title>
                 <Overview>{overview}</Overview>
                 <Title>Genres</Title>
-                <p>{ genres}</p>
+                <p>{genres}</p>
             </MovieInfo>
         </MovieContainer>
-    )
-}
+    );
+};
