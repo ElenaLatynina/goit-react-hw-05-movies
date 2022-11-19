6422211e99b875062d99c7b2ba8a36ffbdfe2a61
ElenaLatynina <latynina.elena.v@gmail.com>
2022-11-19 18:52:32 +0200

    Adeed IMovieItem

---
+++ src/components/MovieItem/MovieItem.jsx
@@ -0,0 +1,20 @@
+import { MovieContainer, Poster, Title, Score, Overview , MovieInfo} from "./MovieItem.styled"
+
+
+export const MovieItem = ({ poster, title, score, overview, genres }) => {
+    return (
+        <MovieContainer>
+            <div>
+            <Poster src={poster } alt={title} width="240"  height="320"/>
+            </div>
+            <MovieInfo>
+                <Title>{title}</Title>
+                <Score>User Score: {score}%</Score>
+                <Title>Overview</Title>
+                <Overview>{overview}</Overview>
+                <Title>Genres</Title>
+                <p>{ genres}</p>
+            </MovieInfo>
+        </MovieContainer>
+    )
+}
---
+++ src/components/MovieItem/MovieItem.styled.js
@@ -0,0 +1,32 @@
+import styled from 'styled-components';
+
+export const Poster = styled.img`
+width:400px;
+`;
+
+export const MovieContainer = styled.div`
+display: flex;
+`;
+
+export const Title = styled.h2`
+font-size:16px;
+margin-bottom:16px;
+`;
+
+export const Score = styled.p`
+  margin-bottom: 15px;
+`;
+
+export const Overview = styled.p`
+  margin-bottom: 15px;
+`;
+
+
+
+
+
+
+export const MovieInfo = styled.div`
+margin-left: 12px;
+
+`;
